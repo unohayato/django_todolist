15e3c749ccd913c4b80214e8da68bdbaa0cc2652
unohayato <mamidori1p1p@gmail.com>
2022-07-02 23:14:45 +0900

    テンプレート表示

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from django import views
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.TodoList.as_view(), name="list"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Todo
 
 # Create your views here.
-def index(request):
-  return HttpResponse("<h1>Hello World</h1>")
+
+class TodoList(ListView):
+  model = Todo
+  context_object_name = "tasks"
+  
+  
