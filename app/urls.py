d235beebc82b1b4bafd11fe658063a39bbd059c1
unohayato <mamidori1p1p@gmail.com>
2022-07-03 17:13:10 +0900

    とりあえず完成

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.TodoList.as_view(), name="list"),
+    path('detail/<int:pk>', views.TodoDetail.as_view(), name="detail"),
+    path('create/', views.TodoCreate.as_view(), name="create"),
+    path("update/<int:pk>", views.TodoUpdate.as_view(), name="update"),
+    path("delete/<int:pk>", views.TodoDelete.as_view(), name="delete"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Todo
 
 # Create your views here.
@@ -8,4 +9,21 @@ class TodoList(ListView):
   model = Todo
   context_object_name = "tasks"
   
+class TodoDetail(DetailView):
+  model = Todo  
+  context_object_name = "task"
   
+class TodoCreate(CreateView):
+  model = Todo
+  fields = "__all__"
+  success_url = reverse_lazy("list")
+
+class TodoUpdate(UpdateView):
+  model = Todo
+  fields = "__all__"
+  success_url = reverse_lazy("list")
+
+class TodoDelete(DeleteView):
+    model = Todo
+    context_object_name = "task"
+    success_url = reverse_lazy("list")
